97e8e21aff2e1639494d20382253f02f02149da5
dpprest <spb0007@ya.ru>
2025-07-21 16:28:49 +0300

    commit

--- SigmaTestApp2/Models/AddressTree.cs
+++ SigmaTestApp2/Models/AddressTree.cs
@@ -17,17 +17,48 @@ namespace SigmaTestApp2.Models
             using var reader = new StreamReader(filePath);
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             csv.Read();
-            while(csv.Read())
+            while (csv.Read())
             {
-                var fullAddress = csv.GetField("");
+                var fullAddress = csv.GetField("Полный адрес: ");
+                var addressParts = fullAddress.Split(',').Select(x => x.Trim()).ToArray();
+                AddToTree(addressParts, csv);
             }
         }
-        public AddressTreeElement FindElement(string[] addressPath);
-        public List<AddressTreeElement> GetAllElements();
-        public List<AddressTreeElement> SearchElementsBySubstring(string substring);
-        public AddressTreeNode FindCommonAncestor(IEnumerable<AddressTreeElement> elements);
-        public int GetNodeLevel(AddressTreeNode node);
-        public int GetElementLevel(AddressTreeElement element);
+        private void AddToTree(string[] addressParts,  CsvReader csv)
+        {
+            var currentNode = _root;
+            string currentPath = "Root";
+            for (int i = 0; i < addressParts.Length; i++)
+            {
+                var segment = addressParts[i];
+                currentPath += $", {segment}";
+                if (i  < addressParts.Length - 1)
+                {
+                    var element = new AddressTreeElement (currentPath, segment, i+1);
+                    foreach (var header in csv.HeaderRecord)
+                    {
+                        if (header != "Полный адрес: ")
+                            element.Metadata[header] = csv.GetField(header);
+                    }
+                    currentNode.Elements[segment] = element;
+                }
+                else //если промежуточный узел
+                {
+                    if (!currentNode.Nodes.ContainsKey(segment))
+                    {
+                        var newNode = new AddressTreeNode(segment, currentPath, i + 1);
+                        currentNode.Nodes[segment] = newNode;
+                    }
+                    currentNode = currentNode.Nodes[segment];
+                }
+            }
+        }
+        //public AddressTreeElement FindElement(string[] addressPath);
+        //public List<AddressTreeElement> GetAllElements();
+        //public List<AddressTreeElement> SearchElementsBySubstring(string substring);
+        //public AddressTreeNode FindCommonAncestor(IEnumerable<AddressTreeElement> elements);
+        //public int GetNodeLevel(AddressTreeNode node);
+        //public int GetElementLevel(AddressTreeElement element);
     }   
 
 }
