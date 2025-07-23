bef378318cb3adf2a212f14c42d19d1310f7e113
dpprest <spb0007@ya.ru>
2025-07-23 15:58:54 +0300

    commit

--- SigmaTestApp2/Models/AddressTree.cs
+++ SigmaTestApp2/Models/AddressTree.cs
@@ -24,7 +24,7 @@ namespace SigmaTestApp2.Models
                 AddToTree(addressParts, csv);
             }
         }
-        private void AddToTree(string[] addressParts,  CsvReader csv)
+        private void AddToTree(string[] addressParts, CsvReader csv)
         {
             var currentNode = _root;
             string currentPath = "Root";
@@ -32,9 +32,9 @@ namespace SigmaTestApp2.Models
             {
                 var segment = addressParts[i];
                 currentPath += $", {segment}";
-                if (i  < addressParts.Length - 1)
+                if (i < addressParts.Length - 1)
                 {
-                    var element = new AddressTreeElement (currentPath, segment, i+1);
+                    var element = new AddressTreeElement(currentPath, segment, i + 1);
                     foreach (var header in csv.HeaderRecord)
                     {
                         if (header != "Полный адрес: ")
@@ -53,12 +53,79 @@ namespace SigmaTestApp2.Models
                 }
             }
         }
-        //public AddressTreeElement FindElement(string[] addressPath);
-        //public List<AddressTreeElement> GetAllElements();
-        //public List<AddressTreeElement> SearchElementsBySubstring(string substring);
-        //public AddressTreeNode FindCommonAncestor(IEnumerable<AddressTreeElement> elements);
-        //public int GetNodeLevel(AddressTreeNode node);
-        //public int GetElementLevel(AddressTreeElement element);
+        public AddressTreeElement FindElement(string[] addressPath)
+        {
+            var currentNode = _root;
+            for (int i = 0; i < addressPath.Length; i++)
+            {
+                var segment = addressPath[i];
+                if (i == addressPath.Length - 1)
+                {
+                    return currentNode.Elements.TryGetValue(segment, out var element) ? element : null;
+                }
+                if (!currentNode.Nodes.TryGetValue(segment, out currentNode))
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+        public List<AddressTreeElement> SearchElementsBySubstring(string substring)
+        {
+            var results = new List<AddressTreeElement>();
+            SearchSubstring(_root, substring, results);
+            return results;
+        }
+        private void SearchSubstring(AddressTreeNode node, string substring, List<AddressTreeElement> results)
+        {
+            foreach (var element in node.Elements.Values)
+            {
+                if (element.OwnSegment.Contains(substring) || element.FullPath.Contains(substring))
+                    results.Add(element);
+            }
+            foreach (var childNode in node.Nodes.Values)
+                SearchSubstring(childNode, substring, results);
+        }
+        public List<AddressTreeElement> GetAllElements()
+        {
+           var elements = new List<AddressTreeElement>();
+           CollectElements(_root, elements);
+           return elements;
+        }
+        private void CollectElements(AddressTreeNode node, List<AddressTreeElement> elements)
+        {
+            elements.AddRange(node.Elements.Values);
+            foreach (var childNode in node.Nodes.Values )
+            {
+                CollectElements(childNode, elements);
+            }
+        }
+        public AddressTreeNode FindCommonAncestor(IEnumerable<AddressTreeElement> elements)
+        {
+            if (elements == null || !elements.Any())
+            {
+                return null;
+            }
+            var paths = elements.Select(e => e.FullPath.Split(", ")).ToList();
+            var minLength = paths.Min(p => p.Length);
+            AddressTreeNode commonAncestor = _root;
+            for (int i = 0; i < minLength; i++)
+            {
+                var currentSegment = paths[0][i];
+                if (paths.Any(p => p[i] != currentSegment))
+                    break;
+                commonAncestor = commonAncestor.Nodes.TryGetValue(currentSegment, out var node) ? node : commonAncestor;
+            }
+                return commonAncestor;
+        }
+        public int GetNodeLevel(AddressTreeNode node)
+        {
+            return node?.Level ?? -1;
+        }
+        public int GetElementLevel(AddressTreeElement element)
+        {
+            return element?.Level ?? -1;
+        }
     }   
 
 }
